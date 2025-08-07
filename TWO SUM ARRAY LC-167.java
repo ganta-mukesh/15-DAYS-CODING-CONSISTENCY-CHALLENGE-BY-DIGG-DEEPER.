9fd1dd8302bc1de00112f416ac237f2c8d55f707
Ganta Mukesh <150810489+ganta-mukesh@users.noreply.github.com>
2025-08-07 21:54:10 +0530

    DAY-6 of the 15-Days Challenge

---
+++ TWO SUM ARRAY LC-167.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int i=0,j=numbers.length-1;
+        while(i<j){
+            if (numbers[i]+numbers[j]==target) return new int []{i+1,j+1};
+            else if((numbers[i]+numbers[j]>target)) j-=1;
+            else i+=1;
+        }
+        return new int[0];
+        }
+    }
